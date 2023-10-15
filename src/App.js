206debb1f47c0e195dd348bed701cc7aee57702f
Iftikhar uddin <iftikhar@connextar.com>
2023-10-15 14:35:29 +0500

    Alert in progress

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import About from "./components/About";
 import {useState} from "react";
+import Alert from "./components/Alert";
 
 function App() {
   const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
   const toggleMode = () => {
       if(mode === "light") {
           setMode("dark");
@@ -20,6 +23,7 @@ function App() {
   return (
     <div>
         <Navbar about="AboutMe" mode={mode} toggleMode={toggleMode}/>
+        <Alert alert="This is a dman alert" />
         <div className="container my-3">
             <TextForm heading="Enter your text "  mode={mode}formTextHeading="use below box to type ..."/>
             {/*<About/>*/}
