f258340eb0a5b7a0d407a700c73298e8edd8be68
Iftikhar uddin <iftipro@gmail.com>
2023-10-16 15:59:10 +0500

    minor fixes

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -47,17 +47,17 @@ function TextForm(props) {
                     <div className="form-group">
                         <label htmlFor="exampleFormControlTextarea1">{props.formTextHeading}</label>
                         <textarea className="form-control" style={{backgroundColor: props.mode === 'light' ? 'white' : 'grey', color: props.mode === 'light' ? 'black' : 'white'}} id="exampleFormControlTextarea1" rows="8" value={text} onChange={handleOnChange}></textarea>
-                        <button className="btn btn-primary mx-1 my-3" onClick={onHandleClick}>Convert To UpperCase</button>
-                        <button className="btn btn-primary mx-1 my-3" onClick={onHandleLowerClick}>Convert To LowerCase</button>
-                        <button className="btn btn-primary mx-1 my-3" onClick={onHandleClearTextClick}>Clear Text</button>
-                        <button className="btn btn-primary mx-1 my-3" onClick={onHandleMakeBoldClick}>Make Text Bold</button>
+                        <button className="btn btn-primary mx-1 my-3 my-1" onClick={onHandleClick}>Convert To UpperCase</button>
+                        <button className="btn btn-primary mx-1 my-3 my-1" onClick={onHandleLowerClick}>Convert To LowerCase</button>
+                        <button className="btn btn-primary mx-1 my-3 my-1" onClick={onHandleClearTextClick}>Clear Text</button>
+                        <button className="btn btn-primary mx-1 my-3 my-1" onClick={onHandleMakeBoldClick}>Make Text Bold</button>
                     </div>
                 </form>
             </div>
 
             <div className="container my-2" style={{color: props.mode === 'light' ? 'black' : 'white'}}>
                <h1>Your text summary</h1>
-                <p>{text.split(" ").length} words & {text.length} characters</p>
+                <p>{text.split(" ").filter((element)=>{return element.length !==0 }).length} words & {text.length} characters</p>
                 <p>{0.008 * text.split(" ").length} minutes to read</p>
                 <h1> Preview </h1>
                 <p>{text.length > 0 ? text : 'Enter text to preview '}</p>
